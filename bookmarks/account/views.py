e94c6aec6bcdc72150598a2b2301f6f781daa143
vlad-terehoff <tereh_off95@mail.ru>
2023-07-18 17:24:01 +0500

    .

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    """Запрос у пользователя данных для регистрации.
+    Потом проверяем введенные им данные на валидность и на активность аккаунта"""
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Аутентификация прошла успешно')
+                else:
+                    return HttpResponse('Отключенная учетная запись')
+            else:
+                return HttpResponse('Недопустимый логин')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})

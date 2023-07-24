76fd84507fb4e709d56f04c985325f08a38cf356
vlad-terehoff <tereh_off95@mail.ru>
2023-07-24 20:23:37 +0500

    .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 #  path('login/', views.user_login, name='login') - старый url входа
 urlpatterns = [path('', views.dashboard, name='dashboard'),
-               path('', include('django.contrib.auth.urls'))
+               path('', include('django.contrib.auth.urls')),
+               path('register/', views.register, name='register'),
+               path('edit/', views.edit, name='edit')
                ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -123,5 +123,6 @@ LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
 

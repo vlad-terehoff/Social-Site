fe084be9604ae0574bbae4dc28e82c8273246268
vlad-terehoff <tereh_off95@mail.ru>
2023-07-26 02:48:27 +0500

    .

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'images.apps.ImagesConfig',
+    'django_extensions',
+    'easy_thumbnails'
 ]
 
 MIDDLEWARE = [
